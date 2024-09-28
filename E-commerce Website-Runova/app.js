f3339885d0fb763de31330bf09f129c11fd2060e
Ginoxo <ginopaulolaso@gmail.com>
2024-09-28 13:46:47 +0800

    Commit mo to

--- E-commerce Website-Runova/app.js
+++ E-commerce Website-Runova/app.js
@@ -6,3 +6,49 @@ window.addEventListener('scroll', function() {
     navbar.classList.remove('scrolled');
   }
 });
+/*about*/
+const images = [
+  "././img/itemsdisplay/shoes-aboutus1.png",
+  "././img/itemsdisplay/shows-aboutus2.png",
+  "././img/itemsdisplay/shoes-aboutus3.png",
+  "././img/itemsdisplay/shoes-aboutus4.png",
+  "././img/itemsdisplay/shoes-aboutus5.png",
+];
+
+let currentIndex = 0;
+
+function changeImage() {
+  const imgElement = document.getElementById("aboutpic");
+  currentIndex = (currentIndex + 1) % images.length;
+  imgElement.src = images[currentIndex];
+}
+
+// Change image every 2 seconds
+setInterval(changeImage, 2000);
+
+const aboutUs = document.getElementById("aboutUs");
+
+function lightningEffect() {
+
+  const randomInterval = Math.random() * 2000 + 500;
+
+  
+  aboutUs.style.opacity = 1;
+  aboutUs.style.color = "#2bb1ff";
+  aboutUs.style.textShadow = "0px 0px 30px white, 0px 0px 50px #2bb1ff";
+
+  // After a short delay, make it dim again (like a lightning flash fading)
+  setTimeout(() => {
+    aboutUs.style.color = "black";
+  }, 100); // Flash lasts 100ms (you can adjust this)
+
+  // Schedule the next flash
+  setTimeout(lightningEffect, randomInterval);
+}
+
+// Start the lightning effect
+lightningEffect();
+
+
+
+/*about ends*/
